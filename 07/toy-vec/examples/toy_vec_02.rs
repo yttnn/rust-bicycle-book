80b50dcf3a9f57e0c9a0644dac39bf348780ab19
yttnn <67893139+yttnn@users.noreply.github.com>
2023-01-06 14:45:12 +0000

    7-9-5

---
+++ 07/toy-vec/examples/toy_vec_02.rs
@@ -0,0 +1,14 @@
+use toy_vec::ToyVec;
+
+fn main() {
+  let e: Option<&String>;
+  {
+    let mut v = ToyVec::new();
+    v.push("Java Finch".to_string());
+    v.push("Budgerigar".to_string());
+
+    e = v.get(1);
+  }
+  //assert_eq!(e, Some(&"Budgerigar".to_string()));
+  // error: `v` does not live long enough   borrowed value does not live long enough
+}
---
+++ 07/toy-vec/src/lib.rs
@@ -0,0 +1,64 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub struct ToyVec<T> {
+    elements: Box<[T]>,
+    len: usize,
+}
+
+impl<T: Default> ToyVec<T> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self { elements: Self::allocate_in_heap(capacity), len: 0, }
+    }
+
+    fn allocate_in_heap(size :usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(size)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.elements.len()
+    }
+
+    pub fn push(&mut self, element: T) {
+        if self.len == self.capacity() {
+            self.grow();
+        }
+        self.elements[self.len] = element;
+        self.len += 1;
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index < self.len {
+            Some(&self.elements[index])
+        } else {
+            None
+        }
+    }
+
+    fn grow(&self) {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
